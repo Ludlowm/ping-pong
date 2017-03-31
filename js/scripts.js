a5244ad6a0b7d0aa272032df8047c26d3d9c6c21
Marko Ludlow <ludlow.marko@gmail.com>
2017-03-31 11:10:38 -0700

    rework all code to show submit button along with numbers counting from userinput.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,39 @@
-var numArray = []
 
 //Front End Logic
-$(document).ready(function(){
-  $("#form").submit(function(event) {
+$(document).ready(function() {
+
+  $("#mainform").submit(function(event){
     event.preventDefault();
-    var userInput = $("#input").val();
-    var result = pingPong(userInput);
-    alert(pingPong);
-    $("#output").text(result);
+
+
+    var numberArray = [];
+    var userInput = parseInt($("#input1").val());
+    var total = 1;
+
+    var tmpNum = "";
+    for (i = 1; i <= userInput; i++) {
+      tmpNum+= i + "<br>";
+    }
+    //show form
+    $(".word-output").show();
+
+    // clear output fields
+    $("#yourStartnum").text("");
+
+    // output starting and calculated values
+    $("#yourStartnum").append(tmpNum);
+
   });
 });
 
 
 
-
-
-
-//Business Logic
-var pingPong = function(result) {
-  for (i = 1; i <= userInput; i++) {
-    if (input % 3 === 0) {
-      numArray.push();
-    }
-  }
-}
+// //Business Logic
+// var pingPong = function(userInput) {
+//   for (i = 1; i <= userInput; i++) {
+//     if (userInput % 3 === 0) {
+//       wholestring.replace("ping");
+//
+//     }
+//   }
+// }
